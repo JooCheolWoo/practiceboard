0bb3df77447a92c6a1f5fe00867011a3add787dd
Pobilover <tkfkdal@naver.com>
2023-04-16 03:10:22 +0900

    게시글 상세 페이지까지 구현함

---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,17 @@
+package com.study.board.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "board")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String content;
+
+}
